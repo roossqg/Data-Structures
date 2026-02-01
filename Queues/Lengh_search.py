7c2bb8097d86e4bd944701e69a797c8eb7a23e26
Neima <a01399122@gmail.com>
2026-01-31 23:42:04 -0300

    some conceitual lengh search

--- Queues/Lengh_search.py
+++ Queues/Lengh_search.py
@@ -8,4 +8,12 @@ class queue():
         #we assume that hash map is sort with names using vector where: key: first stack,item:second stack
         for x in self.hash_names:
             self.arr[-1] = self.hash_names[f'{x}'] # first in - first out
+            #basicaly we push up the next stack for verify actual stack first
+
+    def search(self):
+        for x in self.arr:
+            if self.target in self.arr[x.index][:]:
+                
+                print(f'graph on stack: {x.index}')
+        
             
